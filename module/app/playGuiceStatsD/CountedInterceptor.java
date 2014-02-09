de1836fbec1b04c7c6524b62a56921dc36030b9d
Chanan Braunstein <chanan.braunstein@pearson.com>
2014-02-09 18:29:25 -0500

    totals

--- module/app/playGuiceStatsD/CountedInterceptor.java
+++ module/app/playGuiceStatsD/CountedInterceptor.java
@@ -8,7 +8,9 @@ class CountedInterceptor implements MethodInterceptor {
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		final String statName = invocation.getMethod().getDeclaringClass().getName() + "." + invocation.getMethod().getName();
+		final String combined = "classes.combined.time";
 		Statsd.increment(statName);
+		Statsd.increment(combined);
 		return invocation.proceed();
 	}
 }
--- module/app/playGuiceStatsD/TimedInterceptor.java
+++ module/app/playGuiceStatsD/TimedInterceptor.java
@@ -8,12 +8,14 @@ public class TimedInterceptor implements MethodInterceptor {
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
 		final String statName = invocation.getMethod().getDeclaringClass().getName() + "." + invocation.getMethod().getName();
+		final String combined = "classes.combined.time";
 		final long start = System.currentTimeMillis();
 		
 		final Object ret = invocation.proceed();
 		
 		final long time = System.currentTimeMillis() - start;
 		Statsd.timing(statName, time);
+		Statsd.timing(combined, time);
 		return ret;
 	}
 }
